b5820c611fbf283e2fc823a8dc75581e41e6ebc9
AMMAR <ammarghanem2026@gmail.com>
2026-06-09 19:32:23 +0800

    Create schema.sql
    Add initial database schema

---
+++ database/schema.sql
@@ -0,0 +1,13 @@
+-- FixIt database schema
+
+CREATE DATABASE IF NOT EXISTS fixit_db;
+USE fixit_db;
+
+-- Tables will be added here:
+-- User
+-- ServiceCategory
+-- ProviderProfile
+-- ProviderCategory
+-- Job
+-- Review
+-- Message
